86bb7b9ab87d8a5cc54e3f34b23e773b8cc1d7db
Juan José Aranzales Ochoa <juanjoaranzales@gmail.com>
2023-07-31 11:59:08 -0500

    Add fonts and colors

--- DOM/index.js
+++ DOM/index.js
@@ -2,15 +2,29 @@ function reveal() {
     var reveals = document.querySelectorAll(".reveal");
 
     for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+      // Get the window height
+      var windowHeight = window.innerHeight;
 
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
+      // Get the position of the element from the top of the viewport
+      var elementTop = reveals[i].getBoundingClientRect().top;
+
+      // Set the visible threshold to 150px
+      var elementVisible = 150;
+
+      // If the element is visible, add the "active" class
+      if (elementTop < windowHeight - elementVisible) {
+        showElement(reveals[i]);
+      } else {
+        hideElement(reveals[i]);
+      }
+    }
+
+    function showElement(element) {
+      element.classList.add("active");
+    }
+
+    function hideElement(element) {
+      element.classList.remove("active");
     }
 }
 
